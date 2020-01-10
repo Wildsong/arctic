61f74836d84fe38587a0d61fb30fb1968682b7e2
Brian H Wilson <bwilson@co.clatsop.or.us>
2020-01-10 11:56:13 -0800

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';  // eslint-disable-line no-unused-vars
+import {render} from 'react-dom'
+import App from './App' // eslint-disable-line no-unused-vars
+
+render(
+    <>
+        <App title="arcgis-react"/>
+    </>,
+    document.getElementById("app")
+);
