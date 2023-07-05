a0cf24c2c4d7f2d1f8e7cf2428a274030bba9d9d
Brian Wilson <brian@wildsong.biz>
2023-07-04 22:13:00 -0700

    adding apollo

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -5,7 +5,11 @@ const Footer = () => {
       <>
       <Card>
         <Card.Text>
-            footer
+          Copyright 2023 Brian H Wilson / Wildsong
+        </Card.Text>
+        <Card.Text>
+            I'd like to have a service status message here
+            but maybe it belongs in the individual cards.
         </Card.Text>
       </Card>
       </>
--- client/src/routes/licenses.tsx
+++ client/src/routes/licenses.tsx
@@ -16,6 +16,33 @@ const rows = [
   { id: 2, product: 'ArcMap', user: 'User 3', checkout: '2023-10-11' }
 ];
 
+import { useQuery } from '@apollo/client';
+import { GET_LICENSES, PING } from './queries';
+
+const ShowInstruments = ({ querytype, lastname }) => {
+    const { loading, error, data } = useQuery(GET_INSTRUMENTS, {
+        variables: {querytype,lastname}
+    });
+    if (error) return <p>Error : {error.message}</p>;
+    if (loading) return <p>Loading...</p>;
+  
+    return data.instruments.map(({ id, firstname, lastname, recording_date }) => (
+      <div key={id}>
+        {id} {firstname} {lastname} {recording_date} <br />
+      </div>
+    ));
+}
+
+const Ping = () => {
+    const { loading, error, data } = useQuery(PING);
+    if (loading) return <>Server starting up...</>;
+    if (error) return <>Connect failed: {error.message}</>;
+    return (
+        <font color="lightgrey">{data.ping}</font>
+    );
+}
+
+
 const Licenses = () => {
     return (
         <Card>
@@ -25,6 +52,7 @@ const Licenses = () => {
           <Card.Text>
           <DataGrid columns={columns} rows={rows} />
           </Card.Text>
+          <Ping /><br />
         </Card>
       );
 }
