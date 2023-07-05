652d7aaf1d03b232a7583c56926ef2e68c6dab74
Brian Wilson <brian@wildsong.biz>
2023-07-05 07:12:45 -0700

    added queries

--- client/src/routes/licenses.tsx
+++ client/src/routes/licenses.tsx
@@ -2,6 +2,8 @@ import { Card } from 'react-bootstrap'
 import DataGrid from 'react-data-grid'
 import 'react-data-grid/lib/styles.css'
 // Refer to https://www.npmjs.com/package/react-data-grid
+import { useQuery } from '@apollo/client';
+import { GET_LICENSES, PING } from '../queries';
 
 const columns = [
   { key: 'id', name: 'ID' },
@@ -16,21 +18,20 @@ const rows = [
   { id: 2, product: 'ArcMap', user: 'User 3', checkout: '2023-10-11' }
 ];
 
-import { useQuery } from '@apollo/client';
-import { GET_LICENSES, PING } from './queries';
 
-const ShowInstruments = ({ querytype, lastname }) => {
-    const { loading, error, data } = useQuery(GET_INSTRUMENTS, {
-        variables: {querytype,lastname}
-    });
+const ShowLicenses = () => {
+    const { loading, error, data } = useQuery(GET_LICENSES);
     if (error) return <p>Error : {error.message}</p>;
     if (loading) return <p>Loading...</p>;
-  
-    return data.instruments.map(({ id, firstname, lastname, recording_date }) => (
-      <div key={id}>
-        {id} {firstname} {lastname} {recording_date} <br />
-      </div>
-    ));
+
+    let columns = data.licenses.map( ({id,product,user,checkout}) => {
+        { key: 'id', name: id }
+    });
+    return (
+      <>
+        <DataGrid columns={columns} rows={rows} />
+      </>
+    );
 }
 
 const Ping = () => {
@@ -38,7 +39,9 @@ const Ping = () => {
     if (loading) return <>Server starting up...</>;
     if (error) return <>Connect failed: {error.message}</>;
     return (
+      <>
         <font color="lightgrey">{data.ping}</font>
+      </>
     );
 }
 
@@ -50,7 +53,7 @@ const Licenses = () => {
             ArcGIS Licenses
           </Card.Title>
           <Card.Text>
-          <DataGrid columns={columns} rows={rows} />
+            <ShowLicenses/>
           </Card.Text>
           <Ping /><br />
         </Card>
