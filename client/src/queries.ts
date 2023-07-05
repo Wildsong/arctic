90f6fbba68697311ed55f78ad4c55325b49fca9e
Brian Wilson <brian@wildsong.biz>
2023-07-05 07:13:00 -0700

    added queries

---
+++ client/src/queries.ts
@@ -0,0 +1,26 @@
+/*
+
+FIXME: I would like to code these directly as .graphql files
+but I don't know how to do that yet.
+
+See https://parceljs.org/languages/graphql/
+
+*/
+import { gql } from '@apollo/client'
+
+export const GET_LICENSES = gql`
+    query GetLicenses {
+        licenses {
+            id
+            product
+            user
+            checkout
+        }
+    }
+`;
+
+export const PING = gql`
+    query Ping {
+        timestamp
+    }
+`;
